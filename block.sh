3a17eb0d0b37ec9abc5b7aae07d50b0d8bd04f0e
Adhitya <9362064+kamakshidasan@users.noreply.github.com>
2023-02-18 12:49:38 +0530

    Create block.sh

---
+++ block.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while :; do
+ currenttime=$(date +%H:%M:%S)
+ if [[ "$currenttime" > "21:00:00" ]] || [[ "$currenttime" < "04:30:00" ]]; then
+   pkill -f /Applications/Google Chrome.app/
+   pkill -f /Applications/Self Service.app/
+   pkill -f /Applications/System Settings.app/
+   pkill -f /Applications/Activity Monitor.app/
+   pkill -f /Applications/Utilities/Terminal.app
+
+ else
+   now=$(date +"%T")
+   echo "$now"
+ fi
+ test "$?" -gt 128 && break
+done &
+
+#do shell script "$HOME/login.sh"
