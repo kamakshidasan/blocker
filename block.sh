edbccdf9f9a552de26aad8856543087bddac665d
Adhitya <9362064+kamakshidasan@users.noreply.github.com>
2023-02-28 02:31:36 +0530

    Update block.sh

--- block.sh
+++ block.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 kicktime=$(date +%s)
-resumetime=$(date -v+1H +%s)
-
-echo $resumetime > "$HOME/resume.txt"
 filetime=$( tail -n 1 "$HOME/resume.txt" )
 
 while :; do
  currenttime=$(date +%H:%M:%S)
  resumetime=$(date +%s)
- if [[ "$currenttime" > "21:00:00" ]] || [[ "$currenttime" < "04:00:00" ]] || [[ $resumetime -le $filetime ]]; then
+
+ # think about periyava now and then edits
+ pkill -f /Applications/Safari.app/
+ pkill -f /Applications/Self Service.app/
+
+ #if [[ "$currenttime" > "21:00:00" ]] || [[ "$currenttime" < "04:00:00" ]] || [[ $resumetime -le $filetime ]] || [[ $(pmset -g ps | head -1) =~ "Battery Power" ]]; then
+ if [[ $(pmset -g ps | head -1) =~ "Battery Power" ]]; then
+   /usr/local/bin/dockutil --remove Launchpad
    pkill -f /Applications/Google Chrome.app/
    pkill -f /Applications/Self Service.app/
    pkill -f /Applications/System Settings.app/
@@ -21,10 +25,9 @@ while :; do
    pkill -f /Applications/QuickTime Player.app
    pkill -f /Applications/VLC.app
    pkill -f /System/Applications/Launchpad.app
- else
-   echo "$currenttime"
-   break
+   pkill -f /System/Applications/Preview.app
  fi
+
 done &
 
 #do shell script "$HOME/block.sh"
